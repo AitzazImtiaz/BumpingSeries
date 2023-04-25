e8c622fc219e29f840d9a007087cadb24f2605e9
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-04-25 07:05:45 -0700

    Add files via upload

---
+++ BumpingSeries/BumpingSeries.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <windows.h>
+#include "InfInt.h"
+
+#include <cmath>
+
+#pragma comment(lib, "user32")
+
+#include "InfInt.h"
+
+InfInt power(InfInt base, InfInt exp) {
+    InfInt result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result *= base;
+        }
+        base *= base;
+        exp /= 2;
+    }
+    return result;
+}
+
+InfInt a(InfInt n) {
+    InfInt two = 2;
+    InfInt ceilSqrtN = 1, temp = 1;
+    while (temp * temp <= n) {
+        ceilSqrtN = temp;
+        temp++;
+    }
+    InfInt y = ceilSqrtN * (ceilSqrtN - 1);
+    InfInt z = (n - y - 1 < 0) ? (y - n + 1) : (n - y - 1);
+    InfInt result = (n + 1) * power(two, z) / 2;
+    return result;
+}
+
+int main()
+{
+    ::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+    InfInt n = 1;
+    while(true) {
+        std::cout << a(n);
+        n = n + 1;
+        std::cout << "\n";
+    }
+    std::cin.get();
+}
