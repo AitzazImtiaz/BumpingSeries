4a624bb06721b303d6e6db2e38537e5b81a2f165
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-04-27 02:47:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 n = 1
-while n <= 10:
-    a = ((n + 1) * 2 ** abs(n - (math.ceil(math.sqrt(n)) * (math.ceil(math.sqrt(n)) - 1)) - 1)) / 2
+while True:
+    a = int(((n + 1) * 2 ** abs(n - (math.ceil(math.sqrt(n)) * (math.ceil(math.sqrt(n)) - 1)) - 1)) / 2)
     print(a)
     n += 1
 
